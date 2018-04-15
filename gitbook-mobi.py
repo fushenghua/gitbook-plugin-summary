2cdbcc79a1f12ef227f82beea9d8405226b33480
fushenghua <fushenghua2012@126.com>
2018-04-15 19:10:52 +0800

    Changed:update gitbook-mobi

--- gitbook-mobi.py
+++ gitbook-mobi.py
@@ -5,57 +5,69 @@ import os
 import re
 import json
 
+
 def execute(cmd):
     return os.system(cmd) == 0
+
 
 def mkdir(dirPath):
     if not os.path.exists(dirPath):
         os.makedirs(dirPath)
-        print("mkdir",dirPath) 
+        print("mkdir", dirPath)
+
 
 def readTitle(dirPath):
-    f = open(dirPath+'/book.json', encoding='utf-8') 
-    book = json.load(f)
-    return book['title']     
+    try:
+        f = open(dirPath + '/book.json', encoding='utf-8')
+        book = json.load(f)
+        return book['title']
+    except:
+        return 'book'
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--overwrite', 
-                        help='overwrite on SUMMARY.md', 
-                        action="store_true")
-    parser.add_argument('-a', '--append', 
-                        help='append on SUMMARY.md', 
-                        action="store_true")
-    parser.add_argument('directory', 
-                        help='the directory of your GitBook root')
+    parser.add_argument(
+        '-o',
+        '--overwrite',
+        help='overwrite on SUMMARY.md',
+        action="store_true")
+    parser.add_argument(
+        '-a', '--append', help='append on SUMMARY.md', action="store_true")
+    parser.add_argument('directory', help='the directory of your GitBook root')
     args = parser.parse_args()
     overwrite = args.overwrite
     append = args.append
     dir_input = args.directory
 
     # print information
-    
-    if execute('python3 gitbook-plugin-summary.py -o {}'.format(dir_input)) :
-        print('create summary file:', dir_input, end = ' ')
+    if execute('python3 gitbook-plugin-summary.py {} {}'.format(
+            '-o' if overwrite else '', dir_input)):
+        if overwrite:
+            print('overwrite summary file:', dir_input + '/SUMMARY.md')
+        else:
+            print('create summary file:',
+                  dir_input + '/SUMMARY-GitBook-auto-summary.md')
 
     print('GitBook create start epub:')
-    mkdir(dir_input+'/docs')
-    try:
-        title=readTitle(dir_input)
-    except:
-        title ='book'
+    mkdir(dir_input + '/docs')
 
-    print (title)    
-    if execute('gitbook epub {} {}'.format(dir_input,dir_input+'/docs/'+title+'.epub')):
+    title = readTitle(dir_input)
+
+    print(title)
+    if execute('gitbook epub {} {}'.format(
+            dir_input, dir_input + '/docs/' + title + '.epub')):
         print('*************************************************************')
-        print('GitBook created epub ...:', dir_input, end = ' ')
+        print('GitBook created epub ...:', dir_input)
         print('*************************************************************')
 
-    if execute('./KindleGen_Mac_i386_v2_9/kindlegen {}'.format(dir_input+'/docs/'+title+'.epub')):
-        print('GitBook convert mobi:', dir_input, end = ' ')
+    if execute('./KindleGen_Mac_i386_v2_9/kindlegen {}'.format(
+            dir_input + '/docs/' + title + '.epub')):
+        print('GitBook convert mobi:', dir_input)
 
     print('GitBook generated mobi finished:) ')
     return 0
+
 
 if __name__ == '__main__':
     main()
--- gitbook-plugin-summary.py
+++ gitbook-plugin-summary.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import re
 
+
 def output_markdown(dire, base_dir, output_file, append, iter_depth=0):
     """Main iterator for get information from every file/folder
 
@@ -12,39 +13,40 @@ def output_markdown(dire, base_dir, output_file, append, iter_depth=0):
     p: Judge is directory or is file, then process .md/.markdown files.
     o: write .md information (with identation) to output_file.
     """
-    ignores=['_book','docs','images','node_modules','dict','.git']
+    ignores = ['_book', 'docs', 'images', 'node_modules', 'dict', '.git']
 
-    for filename in sort_dir_file(os.listdir(dire),base_dir): 
+    for filename in sort_dir_file(os.listdir(dire), base_dir):
         # add list and sort
         if filename in ignores:
-            print('continue ', filename) # output log
+            print('continue ', filename)  # output log
             continue
 
-        print('Processing ', filename) # output log
+        print('Processing ', filename)  # output log
         file_or_path = os.path.join(dire, filename)
-        if os.path.isdir(file_or_path): #is dir
+        if os.path.isdir(file_or_path):  #is dir
             if mdfile_in_dir(file_or_path):
                 # if there is .md files in the folder, output folder name
                 # output_file.write('  ' * iter_depth + '* ' + filename + '\n')
-                createRead0(file_or_path,'0-README.md')
-                output_file.write('  ' * iter_depth + 
-                        '* [{}]({}/{})\n'.format(filename,filename,'0-README.md'))
-                output_markdown(file_or_path, base_dir, output_file, append, 
-                                iter_depth + 1) # iteration
-        else: # is file
-            if is_markdown_file(filename): 
-            # re to find target markdown files, $ for matching end of filename
-                if (filename not in ['SUMMARY.md', 
-                                     'SUMMARY-GitBook-auto-summary.md',
-                                     '0-README.md',
-                                     'README.md']):
+                createRead0(file_or_path, '0-README.md')
+                output_file.write('  ' * iter_depth + '* [{}]({}/{})\n'.format(
+                    filename, filename, '0-README.md'))
+                output_markdown(file_or_path, base_dir, output_file, append,
+                                iter_depth + 1)  # iteration
+        else:  # is file
+            if is_markdown_file(filename):
+                # re to find target markdown files, $ for matching end of filename
+                if (filename not in [
+                        'SUMMARY.md', 'SUMMARY-GitBook-auto-summary.md',
+                        '0-README.md', 'README.md'
+                ]):
                     #or iter_depth != 0): # escape SUMMARY.md at base directory
-                    output_file.write('  ' * iter_depth + 
-                        '* [{}]({})\n'.format(write_md_filename(filename, 
-                                                                append), 
-                            os.path.join(os.path.relpath(dire, base_dir), 
-                                         filename)))
+                    output_file.write(
+                        '  ' * iter_depth + '* [{}]({})\n'.format(
+                            write_md_filename(filename, append),
+                            os.path.join(
+                                os.path.relpath(dire, base_dir), filename)))
                     # iter depth for indent, relpath and join to write link.
+
 
 def mdfile_in_dir(dire):
     """判断目录中是否有MD文件
@@ -55,6 +57,7 @@ def mdfile_in_dir(dire):
             if re.search('.md$|.markdown$', filename):
                 return True
     return False
+
 
 def is_markdown_file(filename):
     """ 判断文件名是.Markdown
@@ -69,14 +72,15 @@ def is_markdown_file(filename):
         return filename[:-3]
     elif len(match.group()) is len('.markdown'):
         return filename[:-9]
-        
+
+
 #create 0-README.md
-def createRead0(dir_input,filename):
+def createRead0(dir_input, filename):
     readmeFile = open(os.path.join(dir_input, filename), 'w')
     readmeFile.close()
-    print('createRead0 ', filename) # output log
-  
-   
+    print('createRead0 ', filename)  # output log
+
+
 def sort_dir_file(listdir, dire):
     # sort dirs and files, first files a-z, then dirs a-z
     list_of_file = []
@@ -84,12 +88,13 @@ def sort_dir_file(listdir, dire):
     for filename in listdir:
         if os.path.isdir(os.path.join(dire, filename)):
             list_of_dir.append(filename)
-        else: 
+        else:
             list_of_file.append(filename)
     for dire in list_of_dir:
         list_of_file.append(dire)
-    list_of_file.sort(key = str.lower)  
-    return list_of_file  
+    list_of_file.sort(key=str.lower)
+    return list_of_file
+
 
 def write_md_filename(filename, append):
     """ write markdown filename
@@ -101,43 +106,44 @@ def write_md_filename(filename, append):
     if append:
         for line in former_summary_list:
             if re.search(filename, line):
-                s = re.search('\[.*\]\(',line)
+                s = re.search('\[.*\]\(', line)
                 return s.group()[1:-2]
         else:
             return is_markdown_file(filename)
     else:
         return is_markdown_file(filename)
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--overwrite', 
-                        help='overwrite on SUMMARY.md', 
-                        action="store_true")
-    parser.add_argument('-a', '--append', 
-                        help='append on SUMMARY.md', 
-                        action="store_true")
-    parser.add_argument('directory', 
-                        help='the directory of your GitBook root')
+    parser.add_argument(
+        '-o',
+        '--overwrite',
+        help='overwrite on SUMMARY.md',
+        action="store_true")
+    parser.add_argument(
+        '-a', '--append', help='append on SUMMARY.md', action="store_true")
+    parser.add_argument('directory', help='the directory of your GitBook root')
     args = parser.parse_args()
     overwrite = args.overwrite
     append = args.append
     dir_input = args.directory
 
     # print information
-    print('GitBook auto summary:', dir_input, end = ' ')
+    print('GitBook auto summary:', dir_input, end=' ')
     if overwrite:
-        print('--overwrite', end = ' ')
-    if append and os.path.exists(os.path.join(dir_input, 'SUMMARY.md')): 
+        print('--overwrite', end=' ')
+    if append and os.path.exists(os.path.join(dir_input, 'SUMMARY.md')):
         #append: read former SUMMARY.md
-        print('--append', end = ' ')
+        print('--append', end=' ')
         global former_summary_list
         with open(os.path.join(dir_input, 'SUMMARY.md')) as f:
             former_summary_list = f.readlines()
             f.close()
     print()
     # output to flie
-    if (overwrite == False and 
-        os.path.exists(os.path.join(dir_input, 'SUMMARY.md'))):
+    if (overwrite == False
+            and os.path.exists(os.path.join(dir_input, 'SUMMARY.md'))):
         # overwrite logic
         filename = 'SUMMARY-GitBook-auto-summary.md'
     else:
@@ -149,6 +155,7 @@ def main():
     output.close()
     print('GitBook auto summary finished:) ')
     return 0
+
 
 if __name__ == '__main__':
     main()
